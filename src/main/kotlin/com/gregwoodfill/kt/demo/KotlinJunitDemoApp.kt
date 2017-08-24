b38b12bc3cf723ae7c877feb312a89c49d4efe87
Greg Woodfill <greg@gregwoodfill.com>
2017-08-24 18:32:43 -0500

    add springboot

---
+++ src/main/kotlin/com/gregwoodfill/kt/demo/KotlinJunitDemoApp.kt
@@ -0,0 +1,13 @@
+package com.gregwoodfill.kt.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinJunitDemoApp::class.java, *args)
+}
+
+@SpringBootApplication
+class KotlinJunitDemoApp {
+
+}
